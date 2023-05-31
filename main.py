7d6e87cc36aefc9644d68245d42c4424d9df7cd4
Gouderg <victor.illien974@gmail.com>
2023-05-31 17:34:00 +0200

    update

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from camera import Camera
 from model import CustomModel
 from state import State
 from fileWriter import FileWriter
-import cv2
+import time
 
-def run(poseweights, device, show_output):
+def run(poseweights, device):
 
     camera = Camera()
     model = CustomModel(device, poseweights)
@@ -23,6 +23,7 @@ def run(poseweights, device, show_output):
         qRgb = deviceCamera.getOutputQueue(name="rgb", maxSize=4, blocking=False)
 
         while True:
+            start = time.time()
             inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
 
             orig_image = inRgb.getCvFrame() 
@@ -36,33 +37,9 @@ def run(poseweights, device, show_output):
                 val1, val2 = state.calc()
                 fileW.writeFile([val1, val2])
 
-            colors = [
-                (0, 0, 255),
-                (0, 255, 255),
-                (255, 255, 0),
-                (255, 0, 255),
-                (255, 0, 0)
-            ]
-            if (show_output):
-                radius = 5
-                for (i, elt) in enumerate(results):
-                    cv2.circle(orig_image, (int(elt["x_rs"]), int(elt["y_rs"])), radius, colors[4], -1)
-                    cv2.circle(orig_image, (int(elt["x_ls"]), int(elt["y_ls"])), radius, colors[4], -1)
-                    cv2.circle(orig_image, (int(elt["x_rh"]), int(elt["y_rh"])), radius, colors[4], -1)
-                    cv2.circle(orig_image, (int(elt["x_lh"]), int(elt["y_lh"])), radius, colors[4], -1)
-                    # cv2.rectangle(orig_image, elt["bbox_c1"][0:2], elt["bbox_c1"][2:4], colors[i%5], 2)
-
-                if state.currentBoundingBox != None:
-                    print("printed BBBBBBOX : ", state.currentBoundingBox)
-                    cv2.rectangle(orig_image, state.currentBoundingBox[0:2], state.currentBoundingBox[2:4], (0, 255, 0), 2)
-                
-                cv2.imshow("Render", orig_image)
-                k = cv2.waitKey(33)
-                if k==27: return # Esc key to stop
-
-                print(state.currentBoundingBox)
-
             state.prepareToNextFrame()
+            end = time.time()
+            print("Next frame, time elapsed: ", end-start)
             
             
             
@@ -71,7 +48,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--poseweights', nargs='+', type=str, default='weights/yolov8s-pose.pt', help='model path(s)')
     parser.add_argument('--device', type=str, default='0', help='cpu/0,1,2,3(gpu)')   #device arugments
-    parser.add_argument('--show_output', action=argparse.BooleanOptionalAction)
+    # parser.add_argument('--show_output', action=argparse.BooleanOptionalAction)
     opt = parser.parse_args()
     return opt
 
